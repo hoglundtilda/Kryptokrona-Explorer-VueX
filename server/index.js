524241de6e882c7e0e587bdfb25f87d98ac35f5e
Matilda Höglund <hoglund.matilda@gmail.com>
2020-09-09 10:36:12 +0200

    add server for heroku

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use('/', serveStatic(path.join(__dirname, '../dist')));
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+console.log(`app is listening on port: ${port}`);
